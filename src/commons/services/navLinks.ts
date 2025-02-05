bc251d47221630b8d4543c0fb95ee73f1f91b5df
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-02-05 13:39:49 +0400

    //

--- src/commons/services/navLinks.ts
+++ src/commons/services/navLinks.ts
@@ -1,14 +1,14 @@
 export const navLinks = [
   {
-    link: '/home',
-    title: 'Home',
+    link: "/home",
+    title: "Home",
   },
   {
-    link: '/Shop',
-    title: 'Shop',
+    link: "/shop",
+    title: "Shop",
   },
   {
-    link: '/contact-us',
-    title: 'Contact Us',
+    link: "/contact-us",
+    title: "Contact Us",
   },
 ];
